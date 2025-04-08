cc6d69bfee8d0d81015767f0ecc46faf7a7c9827
Liangyan2024 <liang.yan@telusinternational.com>
2025-04-07 23:57:44 -0400

    update

--- lambda_function.py
+++ lambda_function.py
@@ -4,23 +4,25 @@ import csv
 import os
 from datetime import datetime
 
-client = boto3.client('cognito-idp', region_name='ca-central-1')
-user_pool_id = os.environ.get('Cognito_UserPool_ID')
-
-
-connect_client = boto3.client('connect')
-connect_instance_id = os.environ.get('Amazon_Connect_Instance_ID')
-s3_bucket_name = os.environ.get('S3_Bucket')
-exception_list = [item.strip() for item in os.environ.get('Exception_List').split(',')]
-
-
-s3 = boto3.client('s3')
-
-
 def lambda_handler(event, context):
+    #read environment variable
+    user_pool_id = os.environ.get('Cognito_UserPool_ID')
+    s3_bucket_name = os.environ.get('S3_Bucket')
+    exception_list = [item.strip() for item in os.environ.get('Exception_List').split(',')]
+    connect_instance_id = os.environ.get('Amazon_Connect_Instance_ID')
+
+    final_list = generate_final_delete_list(user_pool_id, connect_instance_id, exception_list)
+
+    if final_list:   
+        upload_logs_s3(s3_bucket_name, final_list)
+
+    return
+    
+
+def generate_final_delete_list(user_pool_id, connect_instance_id, exception_list):
+    
     cognito_user_list = get_cognito_users(user_pool_id)
-    
-    
+        
     connect_user_list = get_connect_users(connect_instance_id)
     
     delete_list1 = generate_delete_list1(cognito_user_list, connect_user_list)
@@ -36,36 +38,31 @@ def lambda_handler(event, context):
     for e in exception_list:
         final_delete_list.pop(e, None)
 
- 
-    if final_delete_list:   
-        upload_logs_s3(s3_bucket_name, final_delete_list)
-
-
-    return
-    
+    return final_delete_list
 
 def get_cognito_users(user_pool_id):
     
     cognito_user_list = {}
     next_token = None
-
+    cognito_client = boto3.client('cognito-idp', region_name='ca-central-1')
     while True:
         
         params = {'UserPoolId': user_pool_id}
         if next_token:
             params['PaginationToken'] = next_token
         
-        response = client.list_users(**params)
+        response = cognito_client.list_users(**params)
 
         for user in response['Users']:
             userInfo = {}
-            userInfo = {'Username': user['Username']}
+            userInfo ['Username'] = user['Username']
             userInfo ['date_created'] = (user['UserCreateDate']).strftime('%Y-%m-%d')
             userInfo ['Last_login']=  (user['UserLastModifiedDate']).strftime('%Y-%m-%d')
             user_id = None
             date_created = None
 
             attr_identity = next((attr for attr in user['Attributes'] if attr['Name'] == 'identities'), None)
+            
 
             if attr_identity:
                 try:
@@ -87,7 +84,7 @@ def get_cognito_users(user_pool_id):
     return cognito_user_list   
 
 def get_connect_users(connect_instance_id):
-    
+    connect_client = boto3.client('connect')
     security_profile_response = connect_client.list_security_profiles(InstanceId=connect_instance_id)
     hierarchy_group_response = connect_client.list_user_hierarchy_groups(InstanceId=connect_instance_id)
     connect_user_list = {}
@@ -164,9 +161,8 @@ def get_connect_users(connect_instance_id):
                             'date_created': created.strftime('%Y-%m-%d'), 
                             'RoutingProfile': profile_name, 
                             'SecurityProfile': ', '.join(security_profile_name), 
-                            'Hierachy': hierarchy_group_name,
-                            'AutoAccept': auto_accept,
-                            'Hierachy': hierarchy_group_name}
+                            'Hierarchy': hierarchy_group_name,
+                            'AutoAccept': auto_accept,}
                 except connect_client.exceptions.ResourceNotFoundException:
                     print(f"User {user['Id']} not found.")
                 except Exception as e:
@@ -187,7 +183,7 @@ def generate_delete_list1(cognito_list, connect_list):
     only_in_connect_key = items2 - items1
     
     for key in only_in_connect_key:
-        if (datetime.today() - datetime.strptime(connect_list[key].get('date_created'), "%Y-%m-%d")).days > 180:
+        if date_since(connect_list[key].get('date_created')) > 180:
             only_in_connect[key] = connect_list[key]
             only_in_connect[key]['delete_reason'] = '1'
     
@@ -197,33 +193,34 @@ def generate_delete_list1(cognito_list, connect_list):
 def generate_delete_list2(cognito_list, connect_list):
 # Mark Connect users with NHT profile as training accounts for deletion if inactive for 1+ month and account is 2+ months old
     nht_delete_list = {}
-    for k,v in connect_list.items():
-        if 'nht' in (v.get('RoutingProfile', '')).lower():
-            if (datetime.today() - datetime.strptime(v.get('date_created'), "%Y-%m-%d")).days > 60:
-                cognito_user = cognito_list.get(k)
+    for login_id,user_data in connect_list.items():
+        if 'nht' in (user_data.get('RoutingProfile', '')).lower():
+            if date_since(user_data.get('date_created')) > 60:
+                cognito_user = cognito_list.get(login_id)
                 if cognito_user:
                     cognito_lastlogin = cognito_user.get('Last_login') 
                     if cognito_lastlogin:
-                        if (datetime.today() - datetime.strptime(cognito_lastlogin, '%Y-%m-%d')).days > 30:
-                            nht_delete_list[k] = v
-                            nht_delete_list[k]['delete_reason'] = '2'
+                        if date_since(cognito_lastlogin) > 30:
+                            nht_delete_list[login_id] = user_data
+                            nht_delete_list[login_id]['delete_reason'] = '2'
 
     return nht_delete_list
 
 def generate_delete_list3(cognito_list, connect_list):
 #Retrieves a dictionary of Amazon Connect users who have not logged into Cognito for the past 6 months (180 days). Only includes users who exist in both lists.
     cognito_inactive_list = {}
-    for k, v in cognito_list.items():
-        if (datetime.today() - datetime.strptime(v.get('Last_login'), "%Y-%m-%d")).days > 180:
-            connect_user = connect_list.get(k)
+    for login_id, user_data in cognito_list.items():
+        if date_since(user_data.get('Last_login')) > 180:
+            connect_user = connect_list.get(login_id)
             if connect_user:
-                cognito_inactive_list[k] = connect_user
-                cognito_inactive_list[k]['delete_reason'] = '3'
+                cognito_inactive_list[login_id] = connect_user
+                cognito_inactive_list[login_id]['delete_reason'] = '3'
 
     return cognito_inactive_list
 
 
 def upload_logs_s3(s3_bucket_name, delete_agent_list):
+    s3_client = boto3.client('s3')
     bucket_name = s3_bucket_name
     csv_filename =  f'{datetime.today().date()}_deleted_agents.csv'
     s3_path = f'logs/{csv_filename}'
@@ -245,11 +242,16 @@ def upload_logs_s3(s3_bucket_name, delete_agent_list):
                 info['last_name'],
                 info['SecurityProfile'],
                 info['RoutingProfile'],
-                info['Hierachy'],
+                info['Hierarchy'],
                 info['AutoAccept'],
                 info['delete_reason']
             ])
-    s3.upload_file(csv_file_path, bucket_name, s3_path)
+    s3_client.upload_file(csv_file_path, bucket_name, s3_path)
 
     print(f"CSV uploaded to s3://{bucket_name}/{s3_path} successful")
         
+
+
+
+def date_since (date):
+    return (datetime.today() - datetime.strptime (date, "%Y-%m-%d")).days
